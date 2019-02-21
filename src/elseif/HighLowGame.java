22bf3acca94e32e81bbd828dbff057a2d51dedf3
ilabstaff <ilabstaff@172.16.116.73>
2019-02-20 19:28:00 -0800

    roboColorChooser_HighLowGame

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,28 +5,39 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(100)+1;
 		
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
+			String dope = JOptionPane.showInputDialog("what is your guess?");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+		    int genji = Integer.parseInt(dope);
 			// 5. if the guess is correct
-				// 6. Win
+		    if (genji == random) {
+				JOptionPane.showMessageDialog(null,"you are correct!!!");
+			}
+				// 6. Win 
+		    
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
 			// 7. if the guess is high
+		    else if (genji>random) {
+				JOptionPane.showMessageDialog(null,"your guess is to high");
+			}
 				// 8. Tell them it's too high
 			// 9. if the guess is low
 				// 10. Tell them it's too low
-
+		    else if (genji<random) {
+				JOptionPane.showMessageDialog(null,"your guess is to low");
+			}
 		// 13. Tell them they lose
 		
 	}
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -14,17 +14,39 @@ public class RobotColorChooser {
 		//1. Create a new Robot
 		Robot bigChungus = new Robot();
 		//3. Ask the user what color they would like the robot to draw
+		for (int i = 0; i < 10001; i++) {
+			
 		
+		String grinch = ""; //JOptionPane.showInputDialog("Which color do you whant the robot to draw? P.S. btw the robot's name is BigChungus!!!");
 		//5. Use an if/else statement to set the pen color that the user requested
-
+		if (grinch.equals("red")) {
+			bigChungus.setPenColor(Color.red);
+		}
+		else if (grinch.equals("blue")){	
+			bigChungus.setPenColor(Color.cyan);
+		}
+		else if (grinch.equals("green")){
+			bigChungus.setPenColor(Color.green);
+		}
+		else if (grinch.equals("orange")) {
+			bigChungus.setPenColor(Color.orange);
+		}
+		else if (grinch.equals("yellow")) {
+			bigChungus.setPenColor(Color.yellow);
+		}
+		else if (grinch.equals("purple")) {
+			bigChungus.setPenColor(Color.magenta);
+		}
         //6. If the user doesn’t enter anything, choose a random color
-
+		if (grinch.equals("")) {
+			bigChungus.setRandomPenColor();
+		}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//4. Set the pen width to 10
-		
+		bigChungus.setPenWidth(10);
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-		bigChungus.setSpeed(100);
+		bigChungus.setSpeed(1000000);
 		bigChungus.penDown();
 		bigChungus.move(100);
 		bigChungus.turn(90);
@@ -33,8 +55,8 @@ public class RobotColorChooser {
 	    bigChungus.move(100);
 	    bigChungus.turn(90);
 	    bigChungus.move(100);
-	    bigChungus.turn(90);
-	    
+	    bigChungus.turn(304);
+		}  
 	}
 
 	
